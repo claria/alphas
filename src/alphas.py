bf7dcfd610c042ec3c1f4099c2cb76985707d37f
Georg Sieber <g.sieber@gmail.com>
2013-07-14 16:50:02 +0200

    added remaining files to repo

---
+++ src/alphas.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+import numpy
+
+import measurement
+
+
+class alphas:
+
+    def __init__(self):
+        pass
+
+
+    def add_measurements(self):
+        pass
+
+    def interpolate_alphas_values(self):
+        pass
+
+
+
+
